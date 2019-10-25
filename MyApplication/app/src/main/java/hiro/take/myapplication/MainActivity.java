6e1917d7001174fb25016c378e24f6d6eb8a9081
v02qabs <takesue090@gmail.com>
2019-10-25 21:43:38 +0900

    change-log-xml

--- MyApplication/app/src/main/java/hiro/take/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/hiro/take/myapplication/MainActivity.java
@@ -1,13 +1,13 @@
-package hiro.take.myapplication;
+//package hiro.take.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+import android.R.*;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_);
     }
 }
